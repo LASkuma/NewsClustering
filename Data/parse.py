4d6e7a0eaf9a9b70f136f08a7178d416a19004ae
chester-desktop <chester.stella@gmail.com>
2013-11-15 23:00:00 -0500

    removing stopwords and stemming

--- Data/parse.py
+++ Data/parse.py
@@ -4,18 +4,31 @@ import string
 
 def main():
     f = open('2012data.txt', 'r')
+    stop = open('english.stop.txt', 'r')
+    stoplist = []
     url = f.readline()
     f.readline()
     doc = []
+    result = []
     table = string.maketrans("","")
     while True:
         line = f.readline().strip()
+        if line.startswith("</Content>"): break
+        doc.append(line)
+    for lines in stop:
+        stoplist.append(lines.strip())
+    texts = [[word for word in document.lower().split() if word not in stoplist]
+             for document in doc]
+    # all_tokens = sum(texts, [])
+    # tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
+    # texts = [[word for word in text if word not in tokens_once]
+    #          for text in texts]
+    for line in texts:
+        line = str(line)
         s = line.translate(table, string.punctuation)
         s = " ".join([stem(word) for word in s.split(" ")])
-        if line.startswith("</Content>"): break
-        doc.append(s.lower())
-
-    print doc
+        result.append(s.lower())
+    print result
 
 if __name__ == "__main__":
     main()
