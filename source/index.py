4c980476198b5ad4a0ab01a8e12351707df823cf
LASkuma <jimmyx@me.com>
2013-11-17 22:18:57 -0500

    Finish indexing

---
+++ source/index.py
@@ -0,0 +1,43 @@
+import logging, gensim, bz2, pickle
+
+def main():
+    mm = gensim.corpora.MmCorpus('../Data/dicts/2012_tfidf.mm')
+    lda = gensim.models.ldamodel.LdaModel.load('../Data/model/2012.lda')
+
+    docs = [lda[doc] for doc in mm]
+
+    index = gensim.similarities.MatrixSimilarity(docs)
+    index.save('../Data/model/2012.index')
+
+    sims = []
+    for i in range(len(docs)):
+        sim = index[docs[i]]
+
+        sim = sorted(enumerate(sim), key=lambda item: -item[1])
+        for doc in sim:
+            if doc[0] == i:
+                sim.remove(doc)
+                break
+        sims.append(sim)
+
+        #print 'For #', i, sim
+
+    pickle.dump(sims, open("../Data/model/sim_index.p", "wb"))
+
+    topics = {}
+    for i in range(20):
+        topics[i] = []
+
+    for i in range(len(docs)):
+        for topic in docs[i]:
+            t = i, topic[1]
+            topics[topic[0]].append(t)
+
+    for num, topic in topics.iteritems():
+        topics[num] = sorted(topic, key=lambda item: -item[1])
+
+    pickle.dump(topics, open("../Data/model/cluster_index.p", "wb"))
+    #print topics
+
+if __name__ == "__main__":
+    main()
--- source/lda.py
+++ source/lda.py
@@ -8,28 +8,11 @@ def main():
 
     lda = gensim.models.ldamodel.LdaModel(corpus = mm, id2word = id2word, 
             num_topics = 20, update_every = 0, passes = 20)
-
     lda.save('../Data/model/2012.lda')
 
-    docs = [lda[doc] for doc in mm]
-
-    index = gensim.similarities.MatrixSimilarity(docs)
-    index.save('../Data/model/2012.index')
-
-    for i in range(len(docs)):
-        sims = index[docs[i]]
-
-        sims = sorted(enumerate(sims), key=lambda item: -item[1])
-        for doc in sims:
-            if doc[0] == i:
-                sims.remove(doc)
-                break
-
-        print 'For #', i, sims
-
-    #topics = lda.show_topics(20)
-    #for topic in topics:
-        #print topic
+    topics = lda.show_topics(20)
+    for topic in topics:
+        print topic
 
 if __name__ == "__main__":
     main()
